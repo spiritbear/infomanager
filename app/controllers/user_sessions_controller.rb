23cbab1bc196505a5cec7f29f0bab5705786f830
Shiv Indap <indap.shiv@gmail.com>
2010-11-28 17:57:55 -0800

    Added Skeleton Code For Create Method

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,6 +3,17 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new
   end
   
+  def create
+    @user_session = UserSession.create(params[:user_session])
+    if @user_session.save
+      flash[:notice] = "Successfully Logged In."
+      redirect_to dashboard_url
+    else
+      flash[:error] = "The credentials you provided were invalid"
+      redirect_to login_url
+    end
+  end
+  
   def destroy
     @user_session = UserSession.find
     @user_session.destroy unless @user_session.blank?

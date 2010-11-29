b11949ba673bf0e79f7b1c0686c4c1cb0d32b0fc
Shiv Indap <indap.shiv@gmail.com>
2010-11-28 17:55:21 -0800

    Added Code for Logout

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,4 +2,11 @@ class UserSessionsController < ApplicationController
   def new
     @user_session = UserSession.new
   end
+  
+  def destroy
+    @user_session = UserSession.find
+    @user_session.destroy unless @user_session.blank?
+    flash[:notice] = "Successfully Logged Out"
+    redirect_to login_url
+  end
 end
